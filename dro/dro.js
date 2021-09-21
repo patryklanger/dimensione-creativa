801078604ccd9e966742fb9975a0e1b31d95016d
Paweł Trojanowski <lewapoel@gmail.com>
2021-09-21 09:26:24 +0200

    .

---
+++ dro/dro.js
@@ -0,0 +1,2 @@
+window.location.href =
+  "https://www.canva.com/design/DAEqSkKpFGs/KgZOtB_ObRHJhfEdsVBdiA/view?utm_content=DAEqSkKpFGs&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
