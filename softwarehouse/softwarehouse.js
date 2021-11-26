2a568ccddac5db99315d78ce95ed1ec2093a345b
Paweł Trojanowski <lewapoel@gmail.com>
2021-11-26 09:25:23 +0100

    .

---
+++ softwarehouse/softwarehouse.js
@@ -0,0 +1,2 @@
+window.location.href =
+  "https://www.canva.com/design/DAEwcWm_fFc/sXOLrDGCLk_8x0aIRHj0jA/view?utm_content=DAEwcWm_fFc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#15";
