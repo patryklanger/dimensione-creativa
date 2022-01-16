f000e11edcccb4d9fec1682b85a659eede80c3f4
Paweł Trojanowski <lewapoel@gmail.com>
2022-01-16 19:09:54 +0100

    .

--- womancode/womancode.js
+++
@@ -1,1 +0,0 @@
-window.location.href = "https://disk.yandex.ru/d/WPHMib8R9CsatQ";
--- woskowijki/woskowijki.js
+++
@@ -1,2 +0,0 @@
-window.location.href =
-  "https://www.canva.com/design/DAEm5gagY7c/o5bD1t9oaL2g5Omun9EM_g/view?utm_content=DAEm5gagY7c&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
