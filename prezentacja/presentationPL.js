5d0b88f58147fcb1e71994f03eddc08f510b4d30
Paweł Trojanowski <lewapoel@gmail.com>
2022-01-27 09:56:49 +0100

    .

--- prezentacja/presentationPL.js
+++ prezentacja/presentationPL.js
@@ -1,2 +1,2 @@
 window.location.href =
-  "https://www.canva.com/design/DAEkq1mk3Cg/dwUVpfdRWoeUFslqlvxlCg/view?utm_content=DAEkq1mk3Cg&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
+  "https://www.canva.com/design/DAE2oD_jKeg/87yy4_Yz8MGrMYMfO8J1nw/view?utm_content=DAE2oD_jKeg&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
