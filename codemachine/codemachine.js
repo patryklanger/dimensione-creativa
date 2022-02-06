237f396b3e0d5720d0e9d61df0c3e44271fd3fb5
Paweł Trojanowski <lewapoel@gmail.com>
2022-02-06 18:55:09 +0100

    .

--- codemachine/codemachine.js
+++
@@ -1,2 +0,0 @@
-window.location.href =
-  "https://www.canva.com/design/DAEqdms2a9k/hRQ9W8TRPEYIRvgUehTESw/view?utm_content=DAEqdms2a9k&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
--- dro/dro.js
+++
@@ -1,2 +0,0 @@
-window.location.href =
-  "https://www.canva.com/design/DAE1nnzq4bg/BuEKlzp8_DIQFlz1kj-RuA/view?utm_content=DAE1nnzq4bg&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
